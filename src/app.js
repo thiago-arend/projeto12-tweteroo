22421c350f5e5982558a380ba4cb8b0cbe8e29d7
thiago-arend <thiago.arend@gmail.com>
2023-06-24 00:00:18 -0300

    criação rota verbo POST '/tweets'

--- src/app.js
+++ src/app.js
@@ -8,14 +8,21 @@ app.use(json());
 
 const usersArray = [];
 
-const tweet = {
-    username: 'bobesponja',
-    avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
-}
+const tweetsArray = [];
 
 app.post("/sign-up", (req, res) => {
-    const {username, avatar} = req.body;
-    usersArray.push({username, avatar});
+    const { username, avatar } = req.body;
+    usersArray.push({ username, avatar });
+
+    res.status(201).send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+    if (!usersArray.find(u => u.username === username)) {
+        return res.status(401).send("UNAUTHORIZED");
+    }
+    tweetsArray.push({ username, tweet });
 
     res.status(201).send("OK");
 });
